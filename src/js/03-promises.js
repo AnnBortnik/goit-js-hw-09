ba21b6fdbed7b471d1c7840d9dafd6204391766a
AnnBortnik <libra.merie.panda@gmail.com>
2023-11-03 12:27:00 +0100

    3 task update

---
+++ src/js/03-promises.js
@@ -0,0 +1,43 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+const notifyOptions = { position: 'center-center', timeout: 10000 };
+
+const formElement = document.querySelector('.form')
+
+formElement.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault()
+    const { delay, step, amount } = event.currentTarget.elements
+
+    let delayValue = Number(delay.value);
+    let stepValue = Number(step.value);
+    let amountValue = Number(amount.value);
+  
+    for (let i = 1; i <= amountValue; i++) {
+      delayValue += stepValue
+      createPromise(i, delayValue)
+        .then(({ position, delay }) => {
+          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, notifyOptions);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, notifyOptions);
+      });
+    }
+  }
+
+  function createPromise(position, delay) {
+    return new Promise(function (resolve, reject) {
+      const shouldResolve = Math.random() > 0.3;
+      const timerId = setTimeout(() => {
+        if (shouldResolve) {
+          // Resolved
+          return resolve({ position, delay })
+        } else {
+          // Rejected
+          return reject({ position, delay })
+        }
+      }, delay)
+    })
+  }
+
+  
